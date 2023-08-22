61fff461af4710d6e762dc025ef0974559c93a33
Mandeep32_1 <mandeepdas321@gmail.com>
2023-08-22 23:12:52 +0530

    first commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,129 @@
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import Layout from "./Layout";
+import NewPost from "./NewPost";
+import Home from "./Home";
+import About from "./About";
+import PostPage from "./PostPage";
+import Missing from "./Missing";
+import EditPost from "./EditPost";
+import { Route, Routes } from "react-router-dom";
+import useAxiosFetch from "./hooks/useAxiosFetch";
+import { useEffect } from "react";
+import { useStoreActions } from "easy-peasy";
 
 function App() {
+  const setPosts = useStoreActions((actions) => actions.setPosts)
+  const { data, isLoading, fetchError } = useAxiosFetch('http://localhost:3500/posts')
+    useEffect(()=>{
+        setPosts(data);
+      },[data, setPosts])
+  // const [posts, setPosts] = useState([]);
+  // const [search, setSearch] = useState("");
+  // const [searchResults, setSearchResults] = useState([]);
+  // const [postTitle, setPostTitle] = useState("");
+  // const [postBody, setPostBody] = useState("");
+  // const [editTitle, setEditTitle] = useState("");
+  // const [editBody, setEditBody] = useState("");
+  // const { width } = useWindowSize();
+  // const { data, fetchError, isLoading } = useAxiosFetch(
+  //   "http://localhost:3500/posts"
+  // );
+  // useEffect(() => {
+  //   const fetchPosts = async () => {
+  //     try {
+  //       const response = await api.get("/posts");
+  //       setPosts(response.data);
+  //     } catch (err) {
+  //       //not in 200 response range
+  //       if (err.response) {
+  //         console.log(err.response.data);
+  //         console.log(err.response.status);
+  //         console.log(err.response.headers);
+  //       } else {
+  //         console.log(`Error: ${err.message}`);
+  //       }
+  //     }
+  //   };
+
+  //   fetchPosts();
+  // }, []);
+
+  // useEffect(() => {
+  //   setPosts(data);
+  // }, [data]);
+
+  // useEffect(() => {
+  //   const filteredResults = posts.filter(
+  //     (post) =>
+  //       post.body.toLowerCase().includes(search.toLowerCase()) ||
+  //       post.title.toLowerCase().includes(search.toLowerCase())
+  //   );
+  //   setSearchResults(filteredResults.reverse());
+  // }, [posts, search]);
+
+  // const handleSubmit = async (e) => {
+  //   e.preventDefault();
+  //   const id = posts.length ? posts[posts.length - 1].id + 1 : 1;
+  //   const datetime = format(new Date(), "MMMM dd, yyyy pp");
+  //   const newPost = { id, title: postTitle, datetime, body: postBody };
+  //   try {
+  //     const response = await api.post("/posts", newPost);
+  //     const allPosts = [...posts, response.data];
+  //     setPosts(allPosts);
+  //     setPostTitle("");
+  //     setPostBody("");
+  //     navigate("/");
+  //   } catch (err) {
+  //     console.log(`Error : ${err.message}`);
+  //   }
+  // };
+
+  // const handleEdit = async (id) => {
+  //   const datetime = format(new Date(), "MMMM dd, yyyy pp");
+  //   const updatedPost = { id, title: editTitle, datetime, body: editBody };
+  //   try {
+  //     const response = await api.put(`/posts/${id}`, updatedPost);
+  //     setPosts(
+  //       posts.map((post) => (post.id === id ? { ...response.data } : post))
+  //     );
+  //     setEditTitle("");
+  //     setEditBody("");
+  //     navigate("/");
+  //   } catch (err) {
+  //     console.log(`Error: ${err.message}`);
+  //   }
+  // };
+
+  // const handleDelete = async (id) => {
+  //   try {
+  //     await api.delete(`/posts/${id}`);
+  //     const postsList = posts.filter((post) => post.id !== id);
+  //     setPosts(postsList);
+  //     navigate("/");
+  //   } catch (err) {
+  //     console.log(`Error : ${err.message}`);
+  //   }
+  // };
+
+  // const navigate = useNavigate();
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+        <Routes>
+          <Route path="/" element={<Layout />}>
+            <Route index element={<Home isLoading={isLoading} fetchError={fetchError} />} />
+            <Route path="post">
+              <Route index element={<NewPost />} />
+              <Route path=":id" element={<PostPage />} />
+            </Route>
+            <Route path="edit/:id">
+              <Route index element={<EditPost />} />
+            </Route>
+
+            <Route path="about" element={<About />} />
+            <Route path="*" element={<Missing />} />
+          </Route>
+        </Routes>     
     </div>
   );
 }
---
+++ src/PostPage.js
@@ -0,0 +1,47 @@
+import React from 'react'
+import { useParams, Link } from 'react-router-dom'
+import { useStoreState, useStoreActions } from 'easy-peasy'
+import { useNavigate } from 'react-router-dom';
+
+function PostPage() {
+  const { id } = useParams()
+  const navigate = useNavigate()
+  const deletePost = useStoreActions((actions) => actions.deletePost)
+  const getPostById = useStoreState((state) => state.getPostById)
+  const post = getPostById(id);
+
+  const handleDelete = (id) => {
+      deletePost(id);
+      navigate("/");
+  };
+  return (
+    <main className='PostPage'>
+        <article className='post'>
+            {
+              post && 
+              <>
+                <h2>{post.title}</h2>
+                <p className='postDate'>{post.datetime}</p>
+                <p className='postBody'>{post.body}</p>
+                <Link to={`/edit/${post.id}`}><button className='editButton'>Edit post</button></Link>
+                <button className="deleteButton" onClick={()=>handleDelete(post.id)}>
+                  Delete Post
+                </button>
+              </>
+            }
+
+            {
+              !post && 
+              <>
+                <h2>Page Not Found</h2>
+                <p>Well, that's disappointing</p>
+                <p><Link to='/'>Visit our homepage</Link></p>
+              </>
+            }
+        </article>
+    </main>
+    
+  )
+}
+
+export default PostPage
